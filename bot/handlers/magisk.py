1fc5292b7e919f6f03b14889f3e71b3476886f80
Hitalo <40531911+hitalosama@users.noreply.github.com>
2021-02-04 23:26:37 -0300

    magisk: added support for magisk canary

--- bot/handlers/magisk.py
+++ bot/handlers/magisk.py
@@ -19,48 +19,52 @@ import rapidjson as json
 from pyrogram import Client, filters
 from pyrogram.types import Message
 
-RAW_URL = "https://github.com/topjohnwu/magisk_files/raw/master"
-TYPES = ["beta", "stable"]
+TYPES = ["beta", "stable", "canary"]
 
 
 @Client.on_message(filters.cmd("magisk"))
 async def on_magisk_m(c: Client, m: Message):
     command = m.text.split()[0]
-    type = m.text[len(command):]
-    
+    type = m.text[len(command) :]
+
     sm = await m.reply("Checking...")
-    
+
     if len(type) < 1:
         type = "stable"
     else:
         type = type[1:]
-        
+
     if not type in TYPES:
-        return await m.reply(f"O tipo de versão <b>{type}</b> não foi encontrado.")
-        
+        return await sm.edit(f"The version type <b>{type}</b> was not found.")
+
+    if type == "canary":
+        RAW_URL = "https://github.com/topjohnwu/magisk_files/raw/canary"
+    else:
+        RAW_URL = "https://github.com/topjohnwu/magisk_files/raw/master"
+
     async with httpx.AsyncClient() as client:
         response = await client.get(f"{RAW_URL}/{type}.json")
         data = json.loads(response.read())
-        
+
     app = data["app"]
     magisk = data["magisk"]
-    
-    text = f"""
-<b>Type</b>: <code>{type}</code>
 
-<b>Manager</b>: <a href="{app['link']}">{app['versionCode']}</a> (v{app['version']})
-<b>Changelog</b>: {await get_changelog(app['note'])}
+    text = f"<b>Type</b>: <code>{type}</code>"
+    if type == "canary":
+        text += f"\n\n<b>Magisk</b>: <a href='https://raw.githubusercontent.com/topjohnwu/magisk_files/canary/app-debug.apk'>{app['versionCode']}</a> (v{app['version']})"
+        text += f"\n<b>Changelog</b>: {await get_changelog('https://github.com/topjohnwu/magisk_files/raw/canary/notes.md')}"
+    else:
+        text += f"<b>\n\nManager</b>: <a href='{app['link']}'>{app['versionCode']}</a> (v{app['version']})"
+        text += f"<b>\nChangelog</b>: {await get_changelog(app['note'])}"
+        text += f"<b>\n\nMagisk</b>: <a href='{magisk['link']}'>{magisk['versionCode']}</a> (v{magisk['version']})"
+        text += f"<b>\nChangelog</b>: {await get_changelog(magisk['note'])}"
+    text += f"\n\n<a href='{data['uninstaller']['link']}'>Uninstaller</a>"
 
-<b>Magisk</b>: <a href="{magisk['link']}">{magisk['versionCode']}</a> (v{magisk['version']})
-<b>Changelog</b>: {await get_changelog(magisk['note'])}
-
-<a href="{data['uninstaller']['link']}">Uninstaller</a>
-    """
     await sm.edit_text(text, disable_web_page_preview=True, parse_mode="combined")
-    
-    
+
+
 async def get_changelog(url: str) -> str:
-    changelog = ''
+    changelog = ""
     async with httpx.AsyncClient() as client:
         response = await client.get(url)
         data = response.read()
