b62ecef82b3af1731ed47f6f557716b895c5dbda
Hitalo <40531911+hitalosama@users.noreply.github.com>
2021-02-07 10:42:45 -0300

    pm_menu: overall changes

--- bot/handlers/pm_menu.py
+++ bot/handlers/pm_menu.py
@@ -14,23 +14,14 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 from pyrogram import Client, filters
-from pyrogram.types import Message
+from pyrogram.types import Message, CallbackQuery
 from pyromod.helpers import ikb
 
 
 @Client.on_message(filters.cmd("start"))
 async def start(c: Client, m: Message):
     keyboard = []
-    text = (
-        "Hi, I'm the <b>official Android Repository Bot</b>, "
-        "here is what I can do for you:\n\n"
-        " - <code>/magisk (type)</code>: Returns the latest version of Magisk.\n"
-        "  > <b>Available Magisk types:</b> <code>stable</code>, <code>beta</code>, <code>canary</code>.\n"
-        " - <code>/twrp (codename)</code>: Return the latest official version of TWRP to the specified device.\n\n"
-        "You can also place requests for the @AndroidRepo staff using the #request in the bot's PM or in the @AndroidRepo_chat.\n\n"
-        "<b>i.e:</b> <code>#request Update the EdXposed module</code>\n\n"
-        "You can request modules, apps, and other files, if you are a developer also feel free to send us your projects."
-    )
+    text = "Hi, I'm the <b>official Android Repository Bot</b>."
     if m.chat.type == "private":
         keyboard.append(
             [
@@ -38,11 +29,12 @@ async def start(c: Client, m: Message):
                 ("📢 Channel", "https://t.me/AndroidRepo", "url"),
             ]
         )
+        keyboard.append([("❔ Help", "help")])
     else:
         keyboard.append(
             [
                 (
-                    "Click here for more info",
+                    "Click here for help!",
                     f"http://t.me/{(await c.get_me()).username}?start",
                     "url",
                 )
@@ -51,4 +43,66 @@ async def start(c: Client, m: Message):
     await m.reply_text(
         text,
         reply_markup=ikb(keyboard),
+    )
+
+
+@Client.on_callback_query(filters.regex("^start_back$"))
+async def start_back(c: Client, m: CallbackQuery):
+    keyboard = ikb(
+        [
+            [
+                ("💬 Group", "https://t.me/AndroidRepo_chat", "url"),
+                ("📢 Channel", "https://t.me/AndroidRepo", "url"),
+            ],
+            [("❔ Help", "help")],
+        ]
+    )
+    await m.message.edit_text(text, reply_markup=keyboard)
+
+
+@Client.on_callback_query(filters.regex("^help$"))
+async def help(c: Client, m: CallbackQuery):
+    keyboard = ikb(
+        [
+            [("🔧 Utilities", "help_commands"), ("💭 Requests", "help_requests")],
+            [("<-", "start_back")],
+        ]
+    )
+    text = "Choose a category for help!"
+    await m.message.edit_text(
+        text,
+        reply_markup=keyboard,
+        disable_web_page_preview=True,
+    )
+
+
+@Client.on_callback_query(filters.regex("^help_requests$"))
+async def help_requests(c: Client, m: CallbackQuery):
+    keyboard = ikb([[("<-", "start_back")]])
+    text = (
+        "<b>Here is what I can do for you:</b>\n\n"
+        "You can also place requests for the @AndroidRepo staff using the #request in the bot's PM or in the @AndroidRepo_chat.\n\n"
+        "<b>i.e:</b> <code>#request Update the EdXposed module</code>\n\n"
+        "You can request modules, apps, and other files, if you are a developer also feel free to send us your projects."
+    )
+    await m.message.edit_text(
+        text,
+        reply_markup=keyboard,
+        disable_web_page_preview=True,
+    )
+
+
+@Client.on_callback_query(filters.regex("^help_commands$"))
+async def help_commands(c: Client, m: CallbackQuery):
+    keyboard = ikb([[("<-", "start_back")]])
+    text = (
+        "<b>Here is what I can do for you:</b>\n\n"
+        " - <code>/magisk (type)</code>: Returns the latest version of Magisk.\n"
+        " - <code>/twrp (codename)</code>: Return the latest official version of TWRP to the specified device.\n\n"
+        "<b>Available Magisk types:</b> <code>stable</code>, <code>beta</code>, <code>canary</code>."
+    )
+    await m.message.edit_text(
+        text,
+        reply_markup=keyboard,
+        disable_web_page_preview=True,
     )
