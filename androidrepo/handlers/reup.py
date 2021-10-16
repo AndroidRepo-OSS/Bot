0aeb95ed95794da5b7b8dcffbf45a39bdee5701b
Hitalo <hitalo331@outlook.com>
2021-10-16 16:10:55 -0300

    reup: using aiodown

--- androidrepo/handlers/reup.py
+++ androidrepo/handlers/reup.py
@@ -1,13 +1,11 @@
 import asyncio
 import html
 import os
-import random
 import re
 import time
-from datetime import datetime, timedelta
+from datetime import datetime
 
-import async_files
-import httpx
+import aiodown
 import humanize
 from pyrogram import filters
 from pyrogram.errors import FloodWait, MessageIdInvalid, MessageNotModified
@@ -15,23 +13,13 @@ from pyrogram.types import Message
 
 from androidrepo.androidrepo import AndroidRepo
 from androidrepo.config import LOGS_ID
-from androidrepo.utils import httpx_timeout
 
 DOWNLOAD_DIR = "./downloads/"
 
 
 @AndroidRepo.on_message(filters.sudo & filters.cmd(r"reup (?P<query>.+)"))
 async def reupload(c: AndroidRepo, m: Message):
-    query = m.matches[0]["query"]
-
-    if "|" in query:
-        file_url, file_name = query.split("|")
-        file_name = file_name.strip()
-    else:
-        file_url = query
-        file_name = None
-
-    file_url = file_url.strip()
+    file_url = m.matches[0]["query"]
 
     is_url = re.match(r"(http(s)?)?(://)?(www)?(\.)?(.*)\.(.*)", file_url)
     if not is_url:
@@ -57,99 +45,28 @@ async def reupload(c: AndroidRepo, m: Message):
 
     start = datetime.now()
 
-    async with httpx.AsyncClient(http2=True, timeout=httpx_timeout) as client:
-        async with client.stream("GET", file_url) as response:
-            num_bytes_total = int(response.headers["Content-Length"])
-            num_bytes_downloaded = response.num_bytes_downloaded
+    file_name = os.path.basename(file_url)
+    file_path = DOWNLOAD_DIR + file_name
 
-            if response.status_code == 200:
-                if not file_name:
-                    file_name = os.path.basename(file_url)
+    async with aiodown.Client() as client:
+        download = client.add(file_url, file_path)
+        await client.start()
 
-                await c.send_log_message(
-                    LOGS_ID,
-                    (
-                        "<b>New re-upload</b>\n"
-                        f"    <b>Sudo:</b> {m.from_user.mention(html.escape(m.from_user.first_name), style='html')}\n"
-                        f"    <b>File Name:</b> <code>{file_name}</code>\n\n"
-                        f"<b>Date:</b> <code>{start.strftime('%H:%M:%S - %d/%m/%Y')}</code>\n#Reupload"
-                    ),
+        last_update = datetime.now()
+        while not download.get_status() == "finished":
+            if download.get_status() == "failed":
+                await m.reply_text("Download failed!")
+                return
+            if (datetime.now() - last_update).seconds >= 3:
+                text = "<b>Downloading...</b>\n"
+                text += f"\n<b>File name</b>: <code>{file_name}</code>"
+                text += f"\n<b>Size</b>: {download.get_size_downloaded(human=True, binary=True)}/{download.get_size_total(human=True, binary=True)}"
+                text += f"\n<b>Speed</b>: {download.get_speed(human=True, binary=True)}"
+                text += (
+                    f"\n<b>Elapsed</b>: {humanize.precisedelta(datetime.now() - start)}"
                 )
-
-                file_path = DOWNLOAD_DIR + file_name
-
-                if not os.path.exists(DOWNLOAD_DIR):
-                    os.mkdir(DOWNLOAD_DIR)
-
-                while os.path.exists(file_path):
-                    file_path += f" ({random.randint(1, 9999)})"
-
-                async with async_files.FileIO(file_path, "wb") as file:
-                    last_update = datetime.now()
-
-                    async for chunk in response.aiter_bytes():
-                        await file.write(chunk)
-
-                        percent = int(
-                            f"{num_bytes_downloaded / num_bytes_total * 100:.0f}"
-                        )
-
-                        if (datetime.now() - last_update).seconds >= 3:
-                            text = "<b>Downloading...</b>\n"
-                            text += f"\n<b>File name</b>: <code>{file_name}</code>"
-                            text += f"\n<b>Size</b>: {humanize.naturalsize(num_bytes_downloaded, binary=True)}/{humanize.naturalsize(num_bytes_total, binary=True)}"
-                            try:
-                                speed = num_bytes_downloaded / (
-                                    (datetime.now() - start).seconds + 1
-                                )
-                            except ZeroDivisionError:
-                                speed = 0
-                            text += f"\n<b>Speed</b>: {humanize.naturalsize(speed, binary=True)}"
-                            text += f"\n<b>Elapsed</b>: {humanize.precisedelta(datetime.now() - start)}"
-                            try:
-                                eta = timedelta(
-                                    seconds=(num_bytes_total - num_bytes_downloaded)
-                                    / speed
-                                )
-                            except ZeroDivisionError:
-                                eta = timedelta(seconds=0)
-                            text += f"\n<b>ETA</b>: {humanize.precisedelta(eta)}"
-                            text += f"\n<b>Progress</b>: {percent}%"
-                            try:
-                                await sent.edit(text)
-                            except FloodWait as e:
-                                await asyncio.sleep(e.x)
-                            except MessageIdInvalid:
-                                sent = await m.reply_text(text)
-                            except MessageNotModified:
-                                pass
-                            last_update = datetime.now()
-
-                        num_bytes_downloaded = response.num_bytes_downloaded
-
-                    await file.close()
-
-            elif response.status_code == 403:
-                await sent.edit("The file has been blocked by the server.")
-            elif response.status_code == 404:
-                await sent.edit("The specified url was not found.")
-            else:
-                await sent.edit(f"<b>Error {response.status_code}</b>")
-
-    if os.path.isfile(file_path):
-        last_edit = 0
-
-        async def progress(current: float, total: float):
-            nonlocal last_edit
-            nonlocal sent
-            percent = int(f"{current / total * 100:.0f}")
-
-            text = "<b>Uploading...</b>\n"
-            text += f"\n<b>File name</b>: <code>{file_name}</code>"
-            text += f"\n<b>Size</b>: {humanize.naturalsize(current)}/{humanize.naturalsize(total)}"
-            text += f"\n<b>Elapsed</b>: {humanize.precisedelta(datetime.now() - start)}"
-            text += f"\n<b>Progress</b>: {percent}%"
-            if last_edit + 1 < int(time.time()) or current == total:
+                text += f"\n<b>ETA</b>: {download.get_eta(human=True, precise=True)}"
+                text += f"\n<b>Progress</b>: {download.get_progress()}%"
                 try:
                     await sent.edit(text)
                 except FloodWait as e:
@@ -158,22 +75,53 @@ async def reupload(c: AndroidRepo, m: Message):
                     sent = await m.reply_text(text)
                 except MessageNotModified:
                     pass
-                last_edit = int(time.time())
+                last_update = datetime.now()
 
-        await c.send_channel_document(
-            caption=file_desc.text.markdown,
-            document=file_path,
-            file_name=file_name,
-            force_document=True,
-            progress=progress,
-            parse_mode="MD",
-        )
+    last_edit = 0
 
-        end = datetime.now()
+    async def progress(current: float, total: float):
+        nonlocal last_edit
+        nonlocal sent
+        percent = int(f"{current / total * 100:.0f}")
 
-        await sent.edit(
-            f"The file was re-uploaded in <code>{(end - start).microseconds / 1000.0:.2f}</code>ms."
-        )
-        os.remove(file_path)
-    else:
-        await sent.edit("The file to upload was not found.")
+        text = "<b>Uploading...</b>\n"
+        text += f"\n<b>File name</b>: <code>{file_name}</code>"
+        text += f"\n<b>Size</b>: {humanize.naturalsize(current)}/{humanize.naturalsize(total)}"
+        text += f"\n<b>Elapsed</b>: {humanize.precisedelta(datetime.now() - start)}"
+        text += f"\n<b>Progress</b>: {percent}%"
+        if last_edit + 1 < int(time.time()) or current == total:
+            try:
+                await sent.edit(text)
+            except FloodWait as e:
+                await asyncio.sleep(e.x)
+            except MessageIdInvalid:
+                sent = await m.reply_text(text)
+            except MessageNotModified:
+                pass
+            last_edit = int(time.time())
+
+    await c.send_channel_document(
+        caption=file_desc.text.markdown,
+        document=file_path,
+        file_name=file_name,
+        force_document=True,
+        progress=progress,
+        parse_mode="MD",
+    )
+
+    end = datetime.now()
+
+    await sent.edit(
+        f"The file was re-uploaded in <code>{(end - start).microseconds / 1000.0:.2f}</code>ms."
+    )
+    os.remove(file_path)
+
+    await c.send_log_message(
+        LOGS_ID,
+        (
+            "<b>New re-upload</b>\n"
+            f"    <b>Sudo:</b> {m.from_user.mention(html.escape(m.from_user.first_name), style='html')}\n"
+            f"    <b>File Name:</b> <code>{file_name}</code>\n\n"
+            f"<b>Date:</b> <code>{start.strftime('%H:%M:%S - %d/%m/%Y')}</code>\n#Reupload"
+        ),
+    )
