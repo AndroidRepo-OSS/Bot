9d03938fa08b3789bf3f51c27263c614c63fd3dc
Hitalo M. <hitalo331@outlook.com>
2025-11-29 11:35:59 -0300

    refactor: resolve allowed updates before starting bot polling

--- src/bot/__main__.py
+++ src/bot/__main__.py
@@ -30,8 +30,8 @@ async def main() -> None:
     register_all(dp, allowed_chat_id=settings.allowed_chat_id, post_topic_id=settings.post_topic_id)
 
     await logger.ainfo("Starting bot...")
-    await bot.delete_webhook(drop_pending_updates=True)
-    await dp.start_polling(bot)
+    allowed_updates = dp.resolve_used_update_types()
+    await dp.start_polling(bot, allowed_updates=allowed_updates)
 
 
 if __name__ == "__main__":
